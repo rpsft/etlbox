67339df839b380b7a7e37a4035ac1029a8d3e3b1
Andreas Lennartz <andreas.lennartz@gmail.com>
2019-11-13 16:04:44 +0100

    Adding prototype MemoryDestination/Source

---
+++ ETLBox/src/Toolbox/DataFlow/MemoryDestination.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ALE.ETLBox.DataFlow
+{
+    /// <summary>
+    /// A destination in memory - it will store all you data in a list. Inserts are done in batches.
+    /// </summary>
+    /// <see cref="MemoryDestination"/>
+    /// <typeparam name="TInput">Type of data input.</typeparam>
+    public class MemoryDestination<TInput> : DataFlowBatchDestination<TInput>, ITask, IDataFlowDestination<TInput>
+    {
+        /* ITask Interface */
+        public override string TaskName => $"Dataflow: Write Data batchwise into memory";
+
+        internal const int DEFAULT_BATCH_SIZE = 1000;
+        public List<TInput> Data { get; set; }
+
+        public MemoryDestination()
+        {
+            BatchSize = DEFAULT_BATCH_SIZE;
+        }
+
+        public MemoryDestination(int batchSize)
+        {
+            BatchSize = batchSize;
+        }
+
+
+        internal override void InitObjects(int batchSize)
+        {
+            base.InitObjects(batchSize);
+        }
+
+        internal override void WriteBatch(ref TInput[] data)
+        {
+            if (Data == null) Data = new List<TInput>(BatchSize);
+            base.WriteBatch(ref data);
+            Data.AddRange(data);
+            LogProgress(data.Length);
+        }
+    }
+
+    /// <summary>
+    /// A destination in memory - it will store all you data in a list. Inserts are done in batches.
+    /// The MemoryDestination access a string array as input type. If you need other data types, use the generic CSVDestination instead.
+    /// </summary>
+    /// <see cref="MemoryDestination{TInput}"/>
+    public class MemoryDestination : MemoryDestination<string[]>
+    {
+        public MemoryDestination() : base() { }
+
+        public MemoryDestination(int batchSize) : base(batchSize) { }
+    }
+}
---
+++ ETLBox/src/Toolbox/DataFlow/MemorySource.cs
@@ -0,0 +1,67 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace ALE.ETLBox.DataFlow
+{
+    /// <summary>
+    /// Reads data from a memory source. While reading the data from the list, data is also asnychronously posted into the targets.
+    /// Data is read a as string from the source and dynamically converted into the corresponding data format.
+    /// </summary>
+    public class MemorySource<TOutput> : DataFlowSource<TOutput>, ITask, IDataFlowSource<TOutput>
+    {
+        /* ITask Interface */
+        public override string TaskName => $"Dataflow: Read CSV Source data from memory";
+
+        /* Public properties */
+        public List<TOutput> Data { get; set; }
+
+        /* Private stuff */
+
+        public MemorySource() : base()
+        {
+            Data = new List<TOutput>();
+        }
+
+        public MemorySource(List<TOutput> data) : base()
+        {
+            Data = data;
+        }
+
+        public override void Execute()
+        {
+            NLogStart();
+            ReadRecordAndSendIntoBuffer();
+            LogProgress(1);
+            Buffer.Complete();
+            NLogFinish();
+        }
+
+        private void ReadRecordAndSendIntoBuffer()
+        {
+            foreach (TOutput record in Data)
+            {
+                Buffer.Post(record);
+            }
+        }
+
+    }
+
+    /// <summary>
+    /// Reads data from a memory source. While reading the data from the file, data is also asnychronously posted into the targets.
+    /// MemorySource as a nongeneric type always return a string array as output. If you need typed output, use
+    /// the MemorySource&lt;TOutput&gt; object instead.
+    /// </summary>
+    /// <see cref="MemorySource{TOutput}"/>
+    public class MemorySource : MemorySource<string[]>
+    {
+        public MemorySource() : base() { }
+        public MemorySource(List<string[]> data) : base(data) { }
+    }
+}
