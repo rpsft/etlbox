25a91ddc0f2c4731980da8cac1815f67f1835415
Andreas Lennartz <andreas.lennartz@gmail.com>
2020-01-02 22:25:01 +0100

    Preparing v1.7.4, changing back to .NET Standard 2.0

--- TestsETLBox/src/DataFlowTests/CSVSource/CSVSourceWithClassMapsTests.cs
+++ TestsETLBox/src/DataFlowTests/CSVSource/CSVSourceWithClassMapsTests.cs
@@ -47,8 +47,8 @@ namespace ALE.ETLBoxTests.DataFlowTests
         public void SimpleFlowUsingClassMap()
         {
             //Arrange
-            TwoColumnsTableFixture dest2Columns = new TwoColumnsTableFixture("CSVDestination2Columns");
-            DBDestination<MySimpleRow> dest = new DBDestination<MySimpleRow>(Connection, "CSVDestination2Columns");
+            TwoColumnsTableFixture dest2Columns = new TwoColumnsTableFixture("CSVDestination2ColumnsClassMap");
+            DBDestination<MySimpleRow> dest = new DBDestination<MySimpleRow>(Connection, "CSVDestination2ColumnsClassMap");
 
             //Act
             CSVSource<MySimpleRow> source = new CSVSource<MySimpleRow>("res/CSVSource/TwoColumns.csv");
@@ -59,6 +59,42 @@ namespace ALE.ETLBoxTests.DataFlowTests
 
             //Assert
             dest2Columns.AssertTestData();
+        }
+
+        public class MyExtendedRow
+        {
+            public string Col2 { get; set; }
+            public long? Col3 { get; set; }
+            public decimal Col4 { get; set; }
+        }
+
+        public class ExtendedClassMap : ClassMap<MyExtendedRow>
+        {
+            public ExtendedClassMap()
+            {
+                Map(m => m.Col2).Index(0);
+                Map(m => m.Col3).Index(1);
+                Map(m => m.Col4).Index(2);
+            }
+        }
+
+
+        [Fact]
+        public void ExtendedFlowUsingClassMap()
+        {
+            //Arrange
+            FourColumnsTableFixture d4c = new FourColumnsTableFixture("CSVDestination4ColumnsClassMap");
+            DBDestination<MyExtendedRow> dest = new DBDestination<MyExtendedRow>(Connection, "CSVDestination4ColumnsClassMap");
+
+            //Act
+            CSVSource<MyExtendedRow> source = new CSVSource<MyExtendedRow>("res/CSVSource/FourColumns.csv");
+            source.Configuration.RegisterClassMap<ExtendedClassMap>();
+            source.LinkTo(dest);
+            source.Execute();
+            dest.Wait();
+
+            //Assert
+            d4c.AssertTestData();
         }
     }
 }
