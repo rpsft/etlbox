e083b69734b071331e58f2d2421347b9d6d45214
SipanOhanyan <54571287+SipanOhanyan@users.noreply.github.com>
2020-05-21 17:00:57 +0400

    Fixed bug :setting the value of the Enum property.

--- ETLBox/src/Helper/PropertyInfoExtension.cs
+++ ETLBox/src/Helper/PropertyInfoExtension.cs
@@ -12,10 +12,19 @@ namespace ALE.ETLBox.Helper
                 throw new ETLBoxException($"Can't write into property {pi?.Name} - property has no setter definition.");
         }
 
-        public static void TrySetValue(this PropertyInfo pi, object obj, object value)
+        public static void TrySetValue(this PropertyInfo pi, object obj, object value, Type enumType = null)
         {
             if (pi.CanWrite)
-                pi.SetValue(obj, value);
+            {
+                if (enumType != null && value != null && enumType.IsEnum)
+                {
+                    pi.SetValue(obj, Enum.Parse(enumType, value?.ToString()));
+                }
+                else
+                {
+                    pi.SetValue(obj, value);
+                }
+            }
         }
     }
 }
