396a33de87baced4dcd323b8816a8ea3fa390936
roadrunnerlenny <andreas.lennartz@gmail.com>
2020-05-26 22:46:48 +0200

    Adding test to check if symbol are excluded for releases

---
+++ TestHelper/src/CheckProjectFiles.cs
@@ -0,0 +1,45 @@
+using ALE.ETLBox;
+using ALE.ETLBox.ConnectionManager;
+using ALE.ETLBox.ControlFlow;
+using ALE.ETLBox.DataFlow;
+using ALE.ETLBox.Helper;
+using ALE.ETLBox.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace ALE.ETLBoxTests
+{
+    public class CheckProjectFiles
+    {
+
+        string noSymbolsPackageXml = $@"
+<PropertyGroup Condition=""'$(Configuration)|$(Platform)'=='Release|AnyCPU'"">
+    <DebugType>none</DebugType>
+    <DebugSymbols>false</DebugSymbols>
+</PropertyGroup>";
+
+        [Fact]
+        public void CheckIfReleasesHaveNoSymbols()
+        {
+            int found = 0;
+            var dirs = Directory.GetDirectories("../../../../");
+            foreach (var dir in dirs)
+            {
+                string cleanedDirName = dir.Replace("../", "").ToLower();
+                if (cleanedDirName.StartsWith("etlbox") && cleanedDirName != "etlboxdocu")
+                {
+                    var projfile = Directory.GetFiles(dir,"*.csproj").First();
+                    var cont = File.ReadAllText(projfile);
+                    Assert.Contains(noSymbolsPackageXml.Replace(" ", ""), cont.Replace(" ", ""));
+                    found++;
+                }
+            }
+
+            Assert.True(found > 0);
+        }
+    }
+}
