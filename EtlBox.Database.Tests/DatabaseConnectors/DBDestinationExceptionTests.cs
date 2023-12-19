7b6c8d92ca816de853d76d78083859217774dcdd
pkudryashov <pkudryashov@rapidsoft.ru>
2023-12-19 19:12:39 +0300

    RSSL-9501 CLONE - EtlBox: добавить возможность безопасно прервать процесс DataFlow

--- EtlBox.Database.Tests/DatabaseConnectors/DBDestinationExceptionTests.cs
+++ EtlBox.Database.Tests/DatabaseConnectors/DBDestinationExceptionTests.cs
@@ -34,7 +34,7 @@ namespace EtlBox.Database.Tests.DatabaseConnectors
             source.LinkTo(dest);
 
             //Act & Assert
-            Assert.Throws<ETLBoxException>(() =>
+            Assert.Throws<InvalidOperationException>(() =>
             {
                 try
                 {
