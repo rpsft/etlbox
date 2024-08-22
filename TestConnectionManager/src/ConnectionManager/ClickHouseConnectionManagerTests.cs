6e675aa3aeb6e8e7debf6045a57b165bd51d7a1d
pkudryashov <pkudryashov>
2024-08-22 16:29:37 +0300

    fix: table metadata read for clickhouse connection manager duplicated
    Case: table column information was double if there was another table with the same name on the other database
    Fix: the information schema select  was corrected

---
+++ TestConnectionManager/src/ConnectionManager/ClickHouseConnectionManagerTests.cs
@@ -0,0 +1,70 @@
+using ALE.ETLBox;
+using ALE.ETLBox.ControlFlow;
+using ETLBox.ClickHouse.ConnectionManager;
+using ETLBox.Primitives;
+using TestShared.Helper;
+
+namespace TestConnectionManager.src.ConnectionManager
+{
+    public class ClickHouseConnectionManagerTests
+    {
+        [Fact]
+        public void TableDefinitionShouldNotDoubles()
+        {
+            // Arrange
+            var connectionString = Config
+                .ClickHouseConnection.ConnectionString("ConnectionManager")
+                .Value;
+
+            var testTable = "test";
+            var dbName1 = "db1";
+            var dbName2 = "db2";
+            var table = new TableDefinition(
+                testTable,
+                new List<TableColumn> { new TableColumn("Id", "Int", false, true) }
+            );
+
+            var builder =
+                new ETLBox.ClickHouse.ConnectionStrings.ClickHouseConnectionStringBuilder();
+            builder.ConnectionString = connectionString;
+
+            var con = new ClickHouseConnectionManager(builder.ConnectionString);
+
+            RecreateDatabase(con, dbName1);
+            RecreateDatabase(con, dbName2);
+
+            builder.Database = dbName1;
+            var con1 = new ClickHouseConnectionManager(builder.ConnectionString);
+            CreateTableTask.Create(con1, table);
+
+            builder.Database = dbName2;
+            var con2 = new ClickHouseConnectionManager(builder.ConnectionString);
+            CreateTableTask.Create(con2, table);
+
+            // Act
+            var dbDefinition = TableDefinition.GetDefinitionFromTableName(con1, table.Name);
+
+            // Assert
+            Assert.NotNull(dbDefinition);
+            Assert.Single(dbDefinition.Columns);
+            Assert.Single(table.Columns[0].Name, dbDefinition.Columns[0].Name);
+        }
+
+        private static void RecreateDatabase(IConnectionManager con, string dbName)
+        {
+            new DropDatabaseTask(dbName)
+            {
+                TaskName = $"Drop database {dbName}",
+                DisableLogging = true,
+                ConnectionManager = con
+            }.DropIfExists();
+
+            new CreateDatabaseTask(dbName)
+            {
+                TaskName = $"Create database {dbName}",
+                DisableLogging = true,
+                ConnectionManager = con
+            }.Execute();
+        }
+    }
+}
